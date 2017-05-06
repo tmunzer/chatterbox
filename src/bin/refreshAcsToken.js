61967e792510673e89351258c0f9097e471e234e
Thomas Munzer <tmunzer@aerohive.com>
2017-05-06 11:07:39 +0200

    removed library icons

--- src/bin/refreshAcsToken.js
+++ src/bin/refreshAcsToken.js
@@ -17,40 +17,41 @@ function refreshOldToken() {
             if (err) console.error("\x1b[31mERROR\x1b[0m:", err);
             // for every selected accounts, try to refresh the token
             else accounts.forEach(function (account) {
-                console.info("\x1b[32minfo\x1b[0m:", "Refreshing token for ownerId " + account.ownerId);
-                OAuth.refreshToken(account.refreshToken, devAccount, function (data) {
-                    // if refresh succeed, update the account data
-                    if (data && data.access_token && data.expires_in && data.refresh_token) {
-                        account.accessToken = data.access_token;
-                        account.expireAt = new Date().valueOf() + (data.expires_in * 1000)
-                        account.refreshToken = data.refresh_token;
-                        console.info("\x1b[32minfo\x1b[0m:", "Token refreshed for ownerId " + account.ownerId);
-                        // if all the needed fields are not received:
-                        // - set the "expireAt" field to 0 (mean it failed, so we will not try this account next time)
-                        // - raise an error
-                    } else {
-                        account.expireAt = 0;
-                        if (data.error) console.error("\x1b[31mERROR\x1b[0m:", "Unable to refresh the token for ownerId " + account.ownerId + " - " + data.error_description);
-                        else console.error("\x1b[31mERROR\x1b[0m:", "Unable to refresh the token for ownerId " + account.ownerId + " - No data received from ACS?");
-                    }
-                    // save the updated account
-                    account.save(function () {
+                if (account.slack == [] && account.spark == []) {
+                    console.info("\x1b[32minfo\x1b[0m:", "Account for ownerId " + account.ownerId + " is no more used. Removing it");
+                    Acs.remove(account, function (err) {
                         if (err) console.error("\x1b[31mERROR\x1b[0m:", err);
-                        else console.info("\x1b[32minfo\x1b[0m:", "Account saved for " + account.ownerId);
                     })
-                })
+                }
+                else {
+                    console.info("\x1b[32minfo\x1b[0m:", "Refreshing token for ownerId " + account.ownerId);
+                    OAuth.refreshToken(account.refreshToken, devAccount, function (data) {
+                        // if refresh succeed, update the account data
+                        if (data && data.access_token && data.expires_in && data.refresh_token) {
+                            account.accessToken = data.access_token;
+                            account.expireAt = new Date().valueOf() + (data.expires_in * 1000)
+                            account.refreshToken = data.refresh_token;
+                            console.info("\x1b[32minfo\x1b[0m:", "Token refreshed for ownerId " + account.ownerId);
+                            // if all the needed fields are not received:
+                            // - set the "expireAt" field to 0 (mean it failed, so we will not try this account next time)
+                            // - raise an error
+                        } else {
+                            account.expireAt = 0;
+                            if (data.error) console.error("\x1b[31mERROR\x1b[0m:", "Unable to refresh the token for ownerId " + account.ownerId + " - " + data.error_description);
+                            else console.error("\x1b[31mERROR\x1b[0m:", "Unable to refresh the token for ownerId " + account.ownerId + " - No data received from ACS?");
+                        }
+                        // save the updated account
+                        account.save(function () {
+                            if (err) console.error("\x1b[31mERROR\x1b[0m:", err);
+                            else console.info("\x1b[32minfo\x1b[0m:", "Account saved for " + account.ownerId);
+                        })
+                    })
+                }
             })
         })
 }
 
 
-function removeOldToken() {
-    console.info("\x1b[32minfo\x1b[0m:", "Starting process to automatically remove unused ACS accounts");
-    Acs
-        .remove({ spark: [], slack: [] }, function (err) {
-            if (err) console.error("\x1b[31mERROR\x1b[0m:", err);
-        })
-}
 //===============CREATE CRON=================
 module.exports.auto = function () {
     // run once when the server is starting
@@ -61,7 +62,6 @@ module.exports.auto = function () {
             // run the refresh process every month
             cronTime: "0 0 0 */1 * *",
             onTick: function () {
-                removeOldToken();
                 refreshOldToken();
             },
             start: true
